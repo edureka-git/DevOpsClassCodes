f5757ae5c477617835596966334e7bc417dd0b19
devopscbabu <108523346+devopscbabu@users.noreply.github.com>
2023-01-16 21:34:52 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region      =   "ap-south-1"
- # access_key  =   "${Access_Key}"
- #  secret_key  =   "${Secret_Key}"
+ access_key  =   "AKIARSATKCBXE42KNO7Z"
+ secret_key  =   "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"
 }
