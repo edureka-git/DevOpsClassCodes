513291789082cc5f2e9cc1d6075cb89af9f2559b
Sravan <sravan.kumar764@gmail.com>
2023-05-09 21:42:02 +0530

    U2

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -60,7 +60,7 @@ public class ContactForm extends FormLayout {
         HorizontalLayout actions = new HorizontalLayout(save, cancel);
         actions.setSpacing(true);
 
-        addComponents(actions, firstName, lastName, phone, email, birthDate);
+        addComponents(actions, firstName, lastName, contactnumber, email, birthDate);
     }
 
     /*
